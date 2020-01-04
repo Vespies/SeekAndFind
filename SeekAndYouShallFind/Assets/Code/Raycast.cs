54b3a1ac53cc54c44f2413cbe9b69f668398ad63
Vespies <43547983+Vespies@users.noreply.github.com>
2020-01-04 12:49:38 +0000

    Raycasting Interactions

---
+++ SeekAndYouShallFind/Assets/Code/Raycast.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Raycast : MonoBehaviour
+{
+    private GameObject raycastObject;
+    [SerializeField]
+    private float rayLength = 10f;
+    [SerializeField]
+    private LayerMask layerMaskInteract;
+    [SerializeField]
+    private Image crosshair;
+    private bool crosshairState;
+    void Start()
+    {
+        crosshairState = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+
+        if (Physics.Raycast (transform.position, forward, out hit, rayLength, layerMaskInteract.value))
+        {
+            if (hit.collider.CompareTag("Interact"))
+            {
+                raycastObject = hit.collider.gameObject;
+                CrosshairActive();
+                crosshairState = true;
+
+                if (Input.GetKeyDown("e"))
+                {
+                    raycastObject.SetActive(false);
+                }
+            }
+        }
+        else
+        {
+            if (crosshairState)
+            {
+                CrosshairInactive();
+            }
+
+        }
+    }
+    void CrosshairActive()
+    {
+        crosshair.color = Color.red;
+    }
+    void CrosshairInactive()
+    {
+        crosshair.color = Color.white;
+        crosshairState = false;
+    }
+}
