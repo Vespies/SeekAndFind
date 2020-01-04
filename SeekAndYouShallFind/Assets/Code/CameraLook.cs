1e93952de1d057dce8d4e6f07e7da4689b760e86
Vespies <43547983+Vespies@users.noreply.github.com>
2020-01-04 05:05:02 +0000

    Movement Mechanic

---
+++ SeekAndYouShallFind/Assets/Code/CameraLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    private float sensitivity = 100f;
+    [SerializeField]
+    private Transform playerBody;
+    [SerializeField]
+    private float rotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        rotation -= mouseY;
+        rotation = Mathf.Clamp(rotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(rotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ SeekAndYouShallFind/Assets/Code/PlayerMovement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private CharacterController controller;
+    [SerializeField]
+    private float speed = 12f;
+    [SerializeField]
+    private Transform feet;
+    [SerializeField]
+    private LayerMask groundMask;
+    private float groundDistance = 0.4f;
+    private float gravity = -10f;
+    private Vector3 velocity;
+    private bool isGrounded;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(feet.position, groundDistance, groundMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        controller.Move(move.normalized * speed * Time.deltaTime);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
